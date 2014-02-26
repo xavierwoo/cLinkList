b201ce23f6126975a9ad73225d59686e894925a5
xavierwoo <xavier.woo@gmail.com>
2014-02-26 15:35:37 +0800

    add some explanations

--- main.c
+++ main.c
@@ -22,26 +22,32 @@ int main(int argc, char *argv[]){
     struct CLink *cl = NULL;
     int *p;
     int i;
-
+    
+    //Create a link list
     l = CLinkList_newList();
     
     for (i=0; i<20; i++) {
         p = (int *)malloc(sizeof(int));
         *p = i;
         
+        //Add an element to the list.
         CLinkList_pushFront(l, p);
     
     }
     cl = l->head;
     
+    //Delete an element.
     CLinkList_delete(cl, free);
     
+    //Print the list to the console.
     CLinkList_printLink(l, pF);
     
+    //Sort the list, "compar" is a compare function like in the qsort.
     CLinkList_sort(l, compar);
     
     CLinkList_printLink(l, pF);
     
+    //The list should be destroyed after use, otherwise there will be memory leaks.
     CLinkList_destroyLink(l, free);
     
     return 0;
